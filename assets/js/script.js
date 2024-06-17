64407ce6aadbe785a7a64710e3643edf51e6b840
Rahul <rahulmandal2k21@gmial.com>
2024-06-17 17:17:45 +0530

    demidi

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,6 +89,12 @@ document.addEventListener('scroll', function() {
 
 
 
+
+
+
+
+
+
 // Onclick button new Tab Open   
 
 function newTabOpen()
@@ -104,7 +110,37 @@ function newTabOpen()
 $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 412,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
     
    
